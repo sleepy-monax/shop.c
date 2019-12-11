4603fd76db2e936de561d8b8b4a654135231f9bf
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-12-11 12:02:03 +0100

    Removed unused functions.

--- source/view/views.h
+++ source/view/views.h
@@ -9,15 +9,7 @@ void user_login(UsersList *users, StockList *stocks, ClientsList *clients);
 
 void home_select_what_todo(User *user, UsersList *users, StockList *stocks, ClientsList *clients);
 
-/* --- Manager -------------------------------------------------------------- */
-
 void manager_select_what_todo(User *user, UsersList *users, StockList *stock, ClientsList *clients);
-
-void mamager_manage_clients(ClientsList *clients);
-
-void manager_manage_stocks(StockList *stock);
-
-/* --- Cashier -------------------------------------------------------------- */
 
 void cashier_select_what_todo(User *user, Basket *basket, StockList *stocks, ClientsList *clients);
 
@@ -26,5 +18,3 @@ void cashier_scan_items(Basket *basket, StockList *stock);
 void cashier_return_consigned_bottles(Basket *basket, StockList *stock);
 
 Client *cashier_input_card_id(ClientsList *clients);
-
-void cashier_create_card_id(Basket *basket, ClientsList *clients);
